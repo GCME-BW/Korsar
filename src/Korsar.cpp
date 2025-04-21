2f1770946357d48e09220a29283cf50dfc9a4687
Andrey <gcme-bw@mailbox.org>
2025-04-21 17:08:39 +0200

    Fixed import issue

--- src/Korsar.cpp
+++ src/Korsar.cpp
@@ -2,7 +2,6 @@
 #include <static_file_manager/static_file_manager.h>
 #include <i2c/i2c_api.h>
 #include <spi/spi_api.h>
-#include <servo/servo_api.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include "LittleFS.h"
@@ -24,13 +23,11 @@ void setup()
 
     setupI2c();
     setupSpi();
-    setupServo();
     setupWifi();
 
     registerStaticFiles(server);
     registerI2cApiMethods(server);
     registerSpiApiMethods(server);
-    registerServoApiMethods(server);
 
     server.begin();
 }
@@ -40,5 +37,4 @@ void loop()
     processWifi();
     processSpiApi();
     processI2cApi();
-    processServoApi();
 }
